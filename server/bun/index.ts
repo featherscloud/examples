653c80aa77bca87f3fe4150c70d6dd1e72a2627e
daffl <daff@neyeon.com>
2024-10-02 11:58:55 -0700

    Run Linter on examples

--- server/bun/index.ts
+++ server/bun/index.ts
@@ -1,55 +1,56 @@
-import { createVerifier } from '@featherscloud/auth';
+import { createVerifier } from '@featherscloud/auth'
 
-const appId = '<your-app-id>';
-const verifier = createVerifier({ appId });
+const appId = '<your-app-id>'
+const verifier = createVerifier({ appId })
 
 const headers = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Headers': '*',
   'Access-Control-Allow-Methods': 'GET',
-  'Access-Control-Allow-Origin': '*'
-};
+  'Access-Control-Allow-Origin': '*',
+}
 
 const server = Bun.serve({
   port: 3030,
   async fetch(request) {
     // Handle CORS preflight request
-		if (request.method === 'OPTIONS') {
-			return new Response(null, {
-				status: 200,
-				headers
-			});
-		}
-	
-		try {
-			const header = request.headers.get('Authorization');
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        status: 200,
+        headers,
+      })
+    }
 
-			if (!header) {
-				throw new Error('Invalid Authorization header');
-			}
+    try {
+      const header = request.headers.get('Authorization')
 
-			// Verify the Authorization header and get the user information
-			const { user } = await verifier.verifyHeader(header);
+      if (!header) {
+        throw new Error('Invalid Authorization header')
+      }
 
-			if (!user) {
-				throw new Error('Cloud auth user not found')
-			}
+      // Verify the Authorization header and get the user information
+      const { user } = await verifier.verifyHeader(header)
 
-			const body = {
-				message: `Hello ${user.email} from BunJS!`
-			};
-	
-			return Response.json(body, {
-				status: 200,
-				headers
-			});
-		} catch (error) {
-			return Response.json({ error: (error as any).message }, {
-				status: 400,
-				headers
-			});
-		}
+      if (!user) {
+        throw new Error('Cloud auth user not found')
+      }
+
+      const body = {
+        message: `Hello ${user.email} from BunJS!`,
+      }
+
+      return Response.json(body, {
+        status: 200,
+        headers,
+      })
+    }
+    catch (error) {
+      return Response.json({ error: (error as any).message }, {
+        status: 400,
+        headers,
+      })
+    }
   },
-});
+})
 
-console.log(`BunJS application ${appId} listening on http://localhost:${server.port}`);
+console.log(`BunJS application ${appId} listening on http://localhost:${server.port}`)
